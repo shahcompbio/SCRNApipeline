6d79d8d6850e835abf96fb780de2304c94d750ae
Ubuntu <nceglia@pre6568.kac5dst41jlebhwmatctq23kma.ux.internal.cloudapp.net>
2019-03-01 20:58:27 +0000

    removing all null rs

--- interface/singlecellexperiment.py
+++ interface/singlecellexperiment.py
@@ -105,7 +105,7 @@ class SingleCellExperiment(RS4):
             return unpacked_object
         for slot in rs4_object.slotnames():
             value = rs4_object.slots[slot]
-            if type(value) == None:
+            if type(value) == NULLTYPE:
                 continue
             elif type(value) == robjects.vectors.ListVector:
                 value = dict(zip(value.names, map(list,list(value))))
