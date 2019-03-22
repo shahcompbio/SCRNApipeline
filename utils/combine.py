e53905e7d767fe558ece019aafb25df248595388
Nick <nickceglia@gmail.com>
2019-03-22 10:52:14 -0400

    removing unused code

--- utils/combine.py
+++
@@ -1,7 +0,0 @@
-from interface.singlecellexperiment import SingleCellExperiment
-
-def combine(sces, filename):
-    combined_sce = SingleCellExperiment.fromRData(sces[0])
-    for sce in sces[1:]:
-        combined_sce = combined_sce + sce
-    combined_sce.save(combined_sce,filename)
