fa182650827782e3da548a8970ee708ca51de4ec
Nick <nickceglia@gmail.com>
2019-04-10 17:39:59 -0400

    results instead of jobs

--- generate_config.py
+++ generate_config.py
@@ -8,7 +8,7 @@ parser = argparse.ArgumentParser()
 
 parser.add_argument('--sampleid', type=str, help='Sample ID linked to fastqs in scrnadata.')
 parser.add_argument("--build", type=str, help="Reference build.", default="GRCh38")
-parser.add_argument("--jobpath", type=str, help="Path to job folder.", default = "/jobs")
+parser.add_argument("--jobpath", type=str, help="Path to job folder.", default = "/results")
 parser.add_argument("--datapath", type=str, help="Path to directory holding fastqs.", default ="/data")
 parser.add_argument("--referencepath", type=str, help="Path to reference directory with reference in dir labelled by build.", default="/reference")
 parser.add_argument("--cellranger", type=str, help="Path to cellranger bin.", default="/codebase/cellranger-3.0.2/cellranger-cs/3.0.2/bin/")
