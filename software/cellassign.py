4e8c2c7801c5f290434cbf534508fc399ddb89e0
Nick <nickceglia@gmail.com>
2019-04-17 11:52:05 -0400

    better rho filtering

--- software/cellassign.py
+++ software/cellassign.py
@@ -49,27 +49,34 @@ rownames(rho) <- rho$X
 rho <- rho[,-1]
 
 sce <- readRDS("{sce}")
+
+cells_to_keep <- sce$pct_counts_mito < 10
+table_cells_to_keep <- table(cells_to_keep)
+sce <- sce[,cells_to_keep]
+rownames(sce) <- rowData(sce)$Symbol
 rho <- as.matrix(rho)
-s <- sizeFactors(sce)
-
-
 counts(sce) <- data.matrix(counts(sce))
 sce <- sce[rowSums(counts(sce)) > 0,]
-common_genes <- intersect(rownames(sce),rownames(rho))
+sce <- sce[,colSums(counts(sce))>0]
+common_genes <- intersect(rowData(sce)$Symbol,rownames(rho))
 sce <- sce[common_genes,]
 rho <- rho[common_genes,]
-rho <- data.matrix(rho)
+sce <- sce[rowSums(counts(sce)) > 0,]
 sce <- sce[,colSums(counts(sce))>0]
+common_genes <- intersect(rowData(sce)$Symbol,rownames(rho))
+sce <- sce[common_genes,]
+rho <- rho[common_genes,]
 
+rho <- data.matrix(rho)
+s <- sizeFactors(sce)
 
 fit_cellassign <- cellassign(exprs_obj = sce, marker_gene_info = rho, s = s, B = 20,shrinkage = TRUE, verbose = TRUE,rel_tol_em = 1e-5, num_runs = 1, min_delta = 2)
-
 
 saveRDS(fit_cellassign, file = '{fname}')"""
 
 
 if __name__ == '__main__':
     rho_yaml = "/work/shah/reference/markers/hgsc_v1.yaml"
-    rdata = "/work/shah/ceglian/Project_09443_D/ABDOM-CD45N_IGO_09443_D_2/runs/.cache/ABDOM-CD45N_IGO_09443_D_2/ABDOM-CD45N_IGO_09443_D_2_qcd.rdata"
+    rdata = "/work/shah/ceglian/Project_09443_D/ABDOM-CD45N_IGO_09443_D_2/runs/.cache/ABDOM-CD45N_IGO_09443_D_2/ABDOM-CD45N_IGO_09443_D_2.rdata"
     results = "cellassignfit.rds"
     CellAssign.run(rdata, rho_yaml, results)
